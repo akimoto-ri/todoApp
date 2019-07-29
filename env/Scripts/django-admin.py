95358f414c3cb6b73070c613f4e9eadf598a6d62
akimoto-ri <47270931+akimoto-ri@users.noreply.github.com>
2019-07-29 14:03:44 +0900

    Add files via upload

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\akimotospecial\study\django-simple-crud-sample-master\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
